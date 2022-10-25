416c0f535de69a01db2515c39a29f5a176c6d789
Aaqil khna <aaqil9755488321@gmail.com>
2022-10-26 02:58:07 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,22 @@
+function input(num){
+   let res = document.getElementById('out');
+    res.value += num;
+    
+}
+function res(){
+    let res = document.getElementById('out');
+    res.value = eval(res.value);
+    return res.value;
+}
+function remove(){
+    let res = document.getElementById('out');
+    res.value = "";
+    return res.value;
+}
+function back(remove){
+    let number = document.getElementById('out');
+    var remove = number.value;
+    remove = remove.slice(0,-1);
+    number.value = remove;
+    return number.value;
+}
